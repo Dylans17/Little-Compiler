6b4a6949da6d983b2ca9653c33b1348156662bad
Dylan Strickley <Dylans17@users.noreply.github.com>
2022-03-25 20:58:19 -0400

    Added FLOAT/INT and function parameters

--- src/SymbolExtractor.java
+++ src/SymbolExtractor.java
@@ -6,67 +6,83 @@ class SymbolExtractor extends LittleBaseListener {
 
 	private Stack<SymbolTable> symbolTableStack;
 	private ArrayList<SymbolTable> allSymbolTables;
+	private String varDeclType;
+	private ArrayList<String> varDeclIdList;
 
 	public SymbolExtractor() {
 		this.symbolTableStack = new Stack<SymbolTable>();
 		this.allSymbolTables = new ArrayList<SymbolTable>();
-		
+
 	}
 
-	@Override public void enterProgram(LittleParser.ProgramContext ctx) {
+	@Override
+	public void enterProgram(LittleParser.ProgramContext ctx) {
 		SymbolTable newSymTab = new SymbolTable("GLOBAL");
 		this.symbolTableStack.push(newSymTab);
 		this.allSymbolTables.add(newSymTab);
 
 	}
 
-	@Override public void exitProgram(LittleParser.ProgramContext ctx) {
+	@Override
+	public void exitProgram(LittleParser.ProgramContext ctx) {
 		//step 3 output
 		for (SymbolTable symTab : allSymbolTables) {
 			symTab.print();
 		}
 	}
 
-	@Override public void enterPgm_body(LittleParser.Pgm_bodyContext ctx) {
-
+	@Override
+	public void enterString_decl(LittleParser.String_declContext ctx) {
+		SymbolTable current = this.symbolTableStack.peek();
+		current.addSymbol(ctx.id().IDENTIFIER().getText(), new SymbolAttributes("STRING",ctx.str().STRINGLITERAL().getText()));
 	}
 
-	@Override public void exitPgm_body(LittleParser.Pgm_bodyContext ctx) {
-
+	@Override
+	public void enterVar_decl(LittleParser.Var_declContext ctx) {
+		varDeclType = ctx.var_type().getText();
+		varDeclIdList = new ArrayList<String>();
 	}
 
-	@Override public void enterDecl(LittleParser.DeclContext ctx) {
+	@Override
+	public void exitVar_decl(LittleParser.Var_declContext ctx) {
+		SymbolTable current = this.symbolTableStack.peek();
 
+		for (String var: varDeclIdList) {
+			current.addSymbol(var, new SymbolAttributes(varDeclType, null));
+		}
+
+		varDeclType = null;
+		varDeclIdList = null;
 	}
 
-	@Override public void exitDecl(LittleParser.DeclContext ctx) {
-
+	@Override
+	public void enterId_list(LittleParser.Id_listContext ctx) {
+		if (varDeclIdList != null)
+			varDeclIdList.add(ctx.id().getText());
 	}
 
-	@Override public void enterString_decl(LittleParser.String_declContext ctx) {
-
-		this.symbolTableStack.peek().addSymbol(ctx.id().IDENTIFIER().getText(), new SymbolAttributes("STRING",ctx.str().STRINGLITERAL().getText()));
-
+	@Override
+	public void enterId_tail(LittleParser.Id_tailContext ctx) {
+		if (ctx.id() != null && varDeclIdList != null)
+			varDeclIdList.add(ctx.id().getText());
 	}
 
-	@Override public void exitString_decl(LittleParser.String_declContext ctx) {
-
+	@Override
+	public void enterParam_decl(LittleParser.Param_declContext ctx) {
+		SymbolTable current = this.symbolTableStack.peek();
+		current.addSymbol(ctx.id().getText(), new SymbolAttributes(ctx.var_type().getText(), null));
 	}
 
-	@Override public void enterVar_decl(LittleParser.Var_declContext ctx) {
-
+	@Override
+	public void enterFunc_decl(LittleParser.Func_declContext ctx) {
+		SymbolTable newSymTab = new SymbolTable(ctx.id().getText());
+		this.symbolTableStack.push(newSymTab);
+		this.allSymbolTables.add(newSymTab);
 	}
 
-	@Override public void exitVar_decl(LittleParser.Var_declContext ctx) {
-
-	}
-
-	@Override public void enterFunc_decl(LittleParser.Func_declContext ctx) {
-
-	}
-
-	@Override public void exitFunc_decl(LittleParser.Func_declContext ctx) {
-
+	@Override
+	public void exitFunc_decl(LittleParser.Func_declContext ctx) {
+		this.symbolTableStack.pop();
 	}
 
 }//end class
@@ -98,13 +114,14 @@ class SymbolTable {
 		this.symbolTable.put(symbolName, attributes);
 		this.symbolNames.add(symbolName);
 	}
-	
+
 	public void print() {
 		System.out.println("Symbol table " + this.scope);
 		for (String symName : symbolNames) {
 			SymbolAttributes attr = this.symbolTable.get(symName);
 			System.out.printf("name %s type %s\n", symName, attr.getType());
 		}
+		System.out.println();
 	}
 
 }//end class
